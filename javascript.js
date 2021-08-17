666089d4558588e34226e0ae479c6569fecd80a0
Luiz F. Santos <lfds.kiko@gmail.com>
2021-08-17 16:22:45 +0200

    second commit 17-08

--- javascript.js
+++
@@ -1,7 +0,0 @@
-
-function setbackground(){
-  window.setTimeout( "setbackground()", 5000);
-  newImage = rotate ? 'url(/images/_LFS8221.jpg)' : 'url(/images/_LFS8170.jpg)';
-  rotate = !rotate;
-  document.getElementById('body').style.backgroundImage = newImage;
-} 
